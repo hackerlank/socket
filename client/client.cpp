b21801ebe805553ad85612b9cb61de421640f8de
izerocrazy <kuangzhenrong@kingsoft.com>
2014-07-30 00:52:31 +0800

    实现部分包裹函数
    1、其实只有一个 close

--- client/client.cpp
+++ client/client.cpp
@@ -36,7 +36,37 @@ int main(nt argc, char **argv)
 	}
 }
 
-// 未完成 Close 函数
+/* 
+包裹函数：这样写是因为，线程函数遇到错误时并不设置标准 Unix 的 errno 变量，而是把 errno 的值作为函数返回值返回给调用者
+那么通常代码就会如下
+int n;
+if ( (n = pthread_mutex_lock (&ndone_mutex)) != 0)
+	errno = n, err_sys("pthread_mutex_lock error");
+
+但，如果我们不需要捕获住特定的某个错误，并且都是以终止进程的方式来处理错误的话，可以将其如下包裹
+
+void Pthread_mutex_lock(pthread_mutex_t *mptr)
+{
+	int n;
+	if( (n = pthread_mutex_lock(mptr)) == 0)
+		return;
+
+	errno = n;
+	err_sys("pthread_mutex_lock error");
+}
+
+这样，调用者只需一句话 Pthread_mutex_lock(&ndone_mutex); 即可处理好错误——对该函数来说，是赋值给 errno，对 socket 系列来说则不用，但 socket 可省略掉终止进程这一步
+
+另外，这种形式也可以用宏来实现，每一个配上一个宏，可以考虑怎么做。同时，我也要记住 coco2d 中，我们常用的两种宏
+*/
+int Close(int sockfd)
+{
+	int n;
+	if ((n = close(sockfd)) < 0)
+		err_sys("socket error");
+
+	return(n);
+}
 
 // 此处的 host name 也可以是 ipaddress，szServName 也可以是 port
 int Tcp_connect(const char* szHostName, const char* szServName)
