6f8bc58d3bfb99a0c68528e027557dd78686a292
izerocrazy <kuangzhenrong@kingsoft.com>
2014-09-19 16:48:04 +0800

    完成 writen

--- base/base.cpp
+++ base/base.cpp
@@ -11,7 +11,25 @@ ssize_t writen(int sockfd, const void* vptr, size_t n)
 {
 	size_t	nleft;
 	ssize_t nwritten;
-	const char
+	const char *ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while(nleft > 0)
+	{
+		if((nwritten = write(sockfd, ptr, nleft)) < 0) {
+			if (nwritten < 0 && errno == EINTR)	// 慢系统调用，继续 write
+				nwritten = 0;
+			else
+				return -1;
+			end	
+		}
+
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+
+	return n;
 }
 
 int Write(int sockfd, const void*vptr, szie_t n)
