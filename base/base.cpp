0c8321275951c70cdd0664279d861279765bde3c
izerocrazy <kuangzhenrong@kingsoft.com>
2014-09-19 18:01:10 +0800

    完成包裹函数 Readn

--- base/base.cpp
+++ base/base.cpp
@@ -34,6 +34,42 @@ ssize_t writen(int sockfd, const void* vptr, size_t n)
 
 int Write(int sockfd, const void*vptr, szie_t n)
 {
-	
+	int nWriten;
+	if ((nWriten = writen(sockfd, vptr, n)) < 0)
+		err_sys("socket error");
+
+	return (nWriten);
 }
 
+ssize_t readn(int sockfd, void* vptr, size_t n)
+{
+	size_t nleft;
+	ssize_t nreadn;
+	char* ptr;
+
+	nleft = n;
+	while (nleft > 0)
+	{
+		if ((nreadn = read(sockfd, ptr, nleft)) < 0) {
+			if errno == EINTR 
+				nreadn = 0;
+			else
+				return -1;
+		} else if (nreadn == 0)
+			break;
+
+		nleft -= nreadn;
+		ptr += nreadn;
+	}
+
+	return n-nleft;		// >= 0
+}
+
+int Readn(int sockfd, void* vptr, size_t n)
+{
+	int nReadn;
+	if ((nReadn = readn(sockfd, vptr, n)) < 0)
+		err_sys("socket err");
+
+	return (nReadn);
+}
